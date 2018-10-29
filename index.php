79e174101223c47f5d14776a10a983c7972f4532
teapardee <tjpardee@gmail.com>
2018-10-29 18:48:28 -0400

    adding titles

--- index.php
+++ index.php
@@ -4,18 +4,33 @@
 
 
 <div id="home-mastgrid">	
-	<p class="home-title">CONSUMER DRIVEN CONSULTING</p>
-	<p class="home-subtitle"></p>
+
+	<div class="home-title-container">
+		<p class="home-title">CONSUMER DRIVEN CONSULTING</p>
+
+			<ul id="home-subtitle-one">
+					<li> [ RECOMMEND ] </li> 
+					<li> [ CONSULT ] </li> 
+					<li> [ IMPLEMENT ] </li> 
+					<li> [ CONSUME ] </li>	
+			</ul>	 
+			<ul id="home-subtitle-two">
+					<li> Submit your feedback, ideas or recommendations. </li>
+					<li> Discuss and develop your ideas with fellow consumers. </li>
+					<li> Work directly with orgs to get your ideas implementated . </li>
+					<li> Consume the products & services you helped tailor. </li>
+			</ul>	
+
+	
+	
 	<div class="home-nav">
-			<a class="home-navlink">Organizations</a><a class="home-arrow"></a>
-			<a class="home-navlink">Organizations</a><a class="home-arrow"></a>
-			<a class="home-navlink">Organizations
-				<div class="home-arrow"></div>
-			</a>
+			<a class="home-navlink">Organizations</a>
+			<a class="home-navlink">Organizations</a>
+			<a class="home-navlink">Organizations</a>
 
 	</div>
 
-
+</div>
 </div>
 
 <?php
