730e43506d20d44659c3be80a698b94ec1b7b5bd
teapardee <tjpardee@gmail.com>
2018-11-01 16:27:31 -0400

    Title

--- index.php
+++ index.php
@@ -4,13 +4,11 @@
 
 
 <div id="home-mastgrid">	
-
-	<div class="home-title-container">
-		<p class="home-title">CONSUMER DRIVEN CONSULTING</p>
-
+<p class="home-title">CONSUMER DRIVEN CONSULTING</p>
+		
 			<ul id="home-subtitle-one">
 					<li> [ RECOMMEND ] </li> 
-					<li> [ CONSULT ] </li> 
+					<li> [ DISCUSS ] </li> 
 					<li> [ IMPLEMENT ] </li> 
 					<li> [ CONSUME ] </li>	
 			</ul>	 
@@ -22,7 +20,6 @@
 			</ul>	
 
 	
-	
 	<div class="home-nav">
 			<a class="home-navlink">Organizations</a>
 			<a class="home-navlink">Organizations</a>
@@ -30,8 +27,54 @@
 
 	</div>
 
+
 </div>
-</div>
+
+<div class="new-grid">
+	<div class="new-header">
+		<p id="headerformatcenter"></p>
+		<p id="headerformatleft"></p>
+	</div>
+	<div class="new-post-container">
+		<div class="new-post">
+			<div id="one" class="new-vote-cell">
+				<div onclick="upvote(this.parentNode.id)" class="new-vote-up"></div>
+				<div onclick="downvote(this.parentNode.id)" class="new-vote-down"></div>
+			</div>
+			<div class="new-post-cell">
+				<div class="new-org">FACEBOOK</div>
+				<div class="new-cat">TIMELINE</div>
+				<div class="new-post-writing">This is a test post to see how things line up and shit like dat.</div>
+			</div>
+				<div class="new-poster-cell">
+					<div class="userformat"><strong>OP</strong>&nbsp;by&nbsp;<strong>TeaPardee</strong>&nbsp;2 hours ago</div>
+					<div class="timeformat"><strong>LP</strong>&nbsp;by&nbsp;<strong>Slayer1993</strong>&nbsp;30 mins ago</div>
+					<a href="index.php" class="new-buttons-1 new-comment-1">COMMENT</a>
+					<a href="index.php" class="new-buttons-1 new-share-1">SHARE</a>
+					<a href="index.php" class="new-buttons-1 new-save-1">SAVE</a>
+				</div>
+		</div>
+		<div class="new-post">
+			<div id="two" class="new-vote-cell">
+				<div onclick="upvote(this.parentNode.id)" class="new-vote-up"></div>
+				<div onclick="downvote(this.parentNode.id)" class="new-vote-down"></div>
+			</div>
+			<div class="new-post-cell">
+				<div class="new-org">NIKE</div>
+				<div class="new-cat">SHOE DESIGN</div>
+				<div class="new-cat">ROSHE</div>
+				<div class="new-post-writing">This is a test post to see how things line up and shit like dat.</div>
+			</div>
+				<div class="new-poster-cell">
+					<div class="userformat"><strong>OP</strong>&nbsp;by&nbsp;<strong>Gommli</strong>&nbsp;8 hours ago</div>
+					<div class="timeformat"><strong>LP</strong>&nbsp;by&nbsp;<strong>GucciMane</strong>&nbsp;2 mins ago</div>
+					<a href="index.php" class="new-buttons-1 new-comment-1">COMMENT</a>
+					<a href="index.php" class="new-buttons-1 new-share-1">SHARE</a>
+					<a href="index.php" class="new-buttons-1 new-save-1">SAVE</a>
+				</div>
+		</div>
+	</div>
+</div>	
 
 <?php
 	include_once 'footer.php';
